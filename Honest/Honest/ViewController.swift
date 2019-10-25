ecac2b500bdfc2ef5cada400d0e2102ec1aac256
Arthur Goldblatt <arthurgoldblatt@berkeley.edu>
2019-10-25 14:46:44 -0700

    created XCode for honest

---
+++ Honest/Honest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Honest
+//
+//  Created by Arthur Goldblatt on 10/25/19.
+//  Copyright © 2019 Arthur Goldblatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # calhacks
 CalHacks 6.0 Project – QR Product Verification App
-lolol
