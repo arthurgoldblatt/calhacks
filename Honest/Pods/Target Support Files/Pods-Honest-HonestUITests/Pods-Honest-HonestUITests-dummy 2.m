15cb2853231954032acfbd5d90846d1f439c590e
Anthony Bajoua <anthonybajoua@berkeley.edu>
2019-10-27 10:32:55 -0700

    firebase working

---
+++ Honest/Pods/Target Support Files/Pods-Honest-HonestUITests/Pods-Honest-HonestUITests-dummy 2.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Honest_HonestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Honest_HonestUITests
+@end
---
+++ Honest/Pods/Target Support Files/Pods-Honest/Pods-Honest-dummy 2.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Honest : NSObject
+@end
+@implementation PodsDummy_Pods_Honest
+@end
